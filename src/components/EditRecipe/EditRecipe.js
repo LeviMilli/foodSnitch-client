fc93e55f985254be9bc9b731dc237d97f462f91a
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-11-03 03:37:37 -0700

    added favicon and edit page loads now

--- src/components/EditRecipe/EditRecipe.js
+++ src/components/EditRecipe/EditRecipe.js
@@ -2,15 +2,18 @@ import axios from 'axios'
 import React from 'react'
 import { useEffect, useState, useContext } from "react";
 import { AppContext } from "../../context/auth.context";
-
+import { Link } from "react-router-dom";
 import { Spinner, Button } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
+import "./EditRecipe.css";
+
+
 // import "./EditRecipe.css";
 
 function EditRecipe() {
     const { 
 
-        user, 
+        user, handleLogout
       }  = useContext(AppContext)
     
     
@@ -48,7 +51,7 @@ function EditRecipe() {
   getData()
 }, [])
 
-console.log(detail)
+// console.log(detail)
 //   useEffect(() => {
 //     axios.get(`http://localhost:5005/api/myrecipe/${id}`, {withCredentials: true})
 //       .then((response) => {
@@ -66,6 +69,7 @@ console.log(detail)
   }
 
   function onEdit(event){
+    console.log(event)
     event.preventDefault()
     handleEdit(id, detail)
   }
@@ -74,18 +78,19 @@ console.log(detail)
     return  <Spinner animation="border" variant="primary" />
   }
 
-
+console.log(detail.RecipeId._id)
   async function handleEdit(id, detail){
-    let response = await axios.patch(`http://localhost:5005/api/myrecipe/${detail.RecipeId}`, detail, {withCredentials: true})
+    let response = await axios.patch(`http://localhost:5005/api/myrecipe/${detail.RecipeId._id}`, detail
+, {withCredentials: true})
 
     let updatedRecipe = detail.RecipeId.map((myrecipe) => {
       if (myrecipe._id === id) {
-        myrecipe.name = response.data.name;
-        myrecipe.description = response.data.description
+        myrecipe.title = response.data.title;
+        myrecipe.readyInMinutes = response.data.readyInMinutes
       }
       return myrecipe
     })
-    console.log(updatedRecipe)
+    // console.log(updatedRecipe)
     setDetail(updatedRecipe)
 
   }
@@ -97,7 +102,7 @@ console.log(detail)
         <input  name="title"  type="text"  defaultValue={detail.RecipeId.title} onChange={handleChange}/>
         
         <label>Image URL</label>
-        <input type="file" name="image" accept="image/png, image/jpg" defaultValue={detail.RecipeId.image}  onChange={handleChange}/>
+        <input type="file" name="image" accept="image/png, image/jpg"  onChange={handleChange}/>
         
         <label>Change ready in minutes</label>
         <input  name="readyInMinutes"  type="number"  defaultValue={detail.RecipeId.readyInMinutes} onChange={handleChange}/>
